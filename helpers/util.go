4072006985046dc9e7aa78a31d98fe9fdfb40703
郝栩彬 <haoxubin@bytedance.com>
2018-09-28 16:48:29 +0800

    1

--- helpers/util.go
+++
@@ -1,9 +0,0 @@
-package helpers
-
-import "math/rand"
-
-func Random(min, max int) int {
-	bettwen := max - min
-	value := rand.Intn(bettwen) + min
-	return value
-}
--- models/response.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type ResponseInfo struct {
-	ErrNO  int         `json:"err_no"`
-	ErrMsg string      `json:"err_msg"`
-	Data   interface{} `json:"data"`
-}
