e28dd936b57f2c2525ee1ffe59c708a8cd5766e9
郝栩彬 <haoxubin@bytedance.com>
2018-09-28 16:54:48 +0800

    feat: 找回main

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package growth
+
+import (
+	"github.com/SayHappy/growth/conf"
+	"github.com/SayHappy/growth/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r:=gin.Default()
+	conf.InitConf()
+	models.InitDB()
+	r.Run(":8080")
+}
